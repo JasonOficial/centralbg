ecc63e3037982152735112d646b05d40c1106aca
jasoncdp <66493121+jasoncdp@users.noreply.github.com>
2020-07-30 16:49:00 -0300

    Add files via upload

--- ajuda.js
+++ ajuda.js
@@ -1,84 +1,130 @@
-//alert("OK!");
-var canvas = document.querySelector("canvas");
-var ctx = canvas.getContext("2d");
+var canvas = document.querySelector('canvas');
+var _txt = document.createElement('h2');
+var box = document.querySelector('#box');
+var ctx = canvas.getContext('2d');
 
-var particles = [];
+var text = [
+    'Jason',
+    'Designer',
+    'Agora o xat.com esta com nova versão html5'
+];
+var objs = [];
+var maxObj = 70;
+var size = 20;
+var count = 0;
 
-var maxObj = 250;
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 window.addEventListener('resize', function(){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}, false);
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+});
 
-function Particle(movRadius, step, position, size){
-    this.movRadius = movRadius;
-    this.step = step;
-    this.position = position;
-    this.size = size;
+box.appendChild(_txt);
+
+function initText(elem){
+    var objs = elem.innerHTML.split('');
+    elem.innerHTML = '';
+    objs.forEach(function(obj, i){
+        setTimeout(function(){
+            elem.innerHTML += obj;
+        }, 175 * i);
+    });
+}
+
+function reading(){
+    text.forEach(function(txt, j){
+        setTimeout(function(){
+            _txt.innerHTML = txt;
+            initText(_txt);
+            count++;
+            
+            if(count === text.length){
+                setTimeout(function(){
+                    count = 0;
+                    this.reading();
+                }, 10000);
+            }
+        }, 10000 * j);
+    });
+}
+
+function Ball(x, y, radius, vx, vy){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.vx = vx;
+    this.vy = vy;
     
     this.draw = function(){
-        var x = Math.cos(this.position) * this.movRadius + canvas.width/2;
-        var y = Math.sin(this.position) * this.movRadius + canvas.height/2;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = '#ffc501';
+        ctx.fill();
         
-        drawStar(x, y, 5, this.size, this.size/2);
-        ctx.fillStyle = 'rgba(255,255,255,.6)';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y + this.radius/4.5, this.radius * 0.6, 0, Math.PI * 0.75, false);
+        ctx.strokeStyle = '#FF4D4D';
+        ctx.stroke();
+        
+        ctx.beginPath();
+        ctx.arc(this.x - this.radius/3, this.y - this.radius/3, this.radius * 0.2, 0, Math.PI * 2);
+        ctx.fillStyle = "#fff";
+        ctx.fill();
+        
+        ctx.beginPath();
+        ctx.arc(this.x + this.radius/3, this.y - this.radius/3, this.radius * 0.2, 0, Math.PI * 2);
+        ctx.fillStyle = "#fff";
+        ctx.fill();
+        
+        ctx.beginPath();
+        ctx.arc(this.x - this.radius/3, this.y - this.radius/3, this.radius * 0.1, 0, Math.PI * 2);
+        ctx.fillStyle = "#000";
+        ctx.fill();
+        
+        ctx.beginPath();
+        ctx.arc(this.x + this.radius/3, this.y - this.radius/3, this.radius * 0.1, 0, Math.PI * 2);
+        ctx.fillStyle = "#000";
         ctx.fill();
     }
     
     this.update = function(){
-        this.position += this.step;
+        if(this.x + this.radius > innerWidth || this.x - this.radius < 0){
+            this.vx = - this.vx;
+        }
+        if(this.y + this.radius > innerHeight || this.y - this.radius < 0){
+            this.vy = -this.vy;
+        }
+        this.x += this.vx;
+        this.y += this.vy;
+        
         this.draw();
     }
 }
 
 function init(){
-    particles = [];
-    for(var i = 0; i < maxObj; i++){
-        var movRadius = Math.random() * canvas.width;
-        var step = (Math.random() * 0.002) + 0.002;
-        var position = Math.random() * (Math.PI*2);
-        var size = (Math.random() * 8) + 0.5;
+    objs = [];
+    for(var i = 0; i < 50; i++){
+        var radius = Math.floor(10 + Math.random() * 25);
+        var x = Math.random() * (innerWidth - radius * 2);
+        var y = Math.random() * (innerHeight - radius * 2);
+        var vx = (Math.random() - 0.5);
+        var vy = (Math.random() - 0.5);
         
-        particles.push(new Particle(movRadius, step, position, size));
+        objs.push(new Ball(x, y, radius, vx, vy));
     }
 }
 
-function loop(){
-    requestAnimationFrame(loop);
+function animate(){
+    requestAnimationFrame(animate);
     ctx.clearRect(0, 0, innerWidth, innerHeight);
-    
-    for(var i in particles){
-        var ptc = particles[i];
-        ptc.update();
+    for(var i in objs){
+        var obj = objs[i];
+        obj.update();
     }
+    
 }
-
 init();
-loop();
-
-function drawStar(px, py, spikes, outRadius, innerRadius){
-    var rotation = Math.PI/2 * 3;
-    var x = px;
-    var y = py;
-    var step = Math.PI / spikes;
-    
-    ctx.beginPath();
-    ctx.moveTo(px, py - outRadius);
-    for(var i = 0; i < spikes; i++){
-        x = px + Math.cos(rotation) * outRadius;
-        y = py + Math.sin(rotation) * outRadius;
-        ctx.lineTo(x,y);
-        rotation += step;
-        
-        x = px + Math.cos(rotation) * innerRadius;
-        y = py + Math.sin(rotation) * innerRadius;
-        ctx.lineTo(x,y);
-        rotation += step;
-    }
-    ctx.lineTo(px, py - outRadius);
-    ctx.closePath();
-}
+animate();
+reading();
